6f459bb6ea6731767447a9847f29cc18ee9f9b24
Dinh Dung <dungdung23092002@gmail.com>
2023-11-14 20:51:52 +0700

    This is first commit

---
+++ src/main/java/guru/framework/backendecommerce/BackEndEcommerceApplication.java
@@ -0,0 +1,13 @@
+package guru.framework.backendecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/framework/backendecommerce/BackEndEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.framework.backendecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
